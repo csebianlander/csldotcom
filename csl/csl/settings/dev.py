ef9ce4286e277051b4f76d11077ca723049d39b5
Chuck Sebian-Lander <csebianlander@gmail.com>
2022-06-15 13:30:07 -0400

    post-wagtail install commit

---
+++ csl/csl/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-*3e(#z7wo2t=ky$ep8_#z4!bbr5_1z6i+f4$28f97rki372up7"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
